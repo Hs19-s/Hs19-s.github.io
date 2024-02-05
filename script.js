14ca7ed236f3cc5998d3995263666c9074d7999d
Heet Shah <158863781+Hs19-s@users.noreply.github.com>
2024-02-05 09:43:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const qs = (selector) => document.querySelector(selector);
+const question = qs(".question");
+const gif = qs(".gif");
+const [yesBtn, noBtn] = [".yes-btn", ".no-btn"].map(qs);
+
+const handleYesClick = () => {
+  question.innerHTML = "Yeahhhhh Yuuutttiii! ♫My Baby My Valentine♫!!";
+  gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+
+  // Remove the 'mouseover' event listener from noBtn
+  noBtn.removeEventListener("mouseover", handleNoMouseOver);
+
+  // Remove the noBtn from the DOM
+  noBtn.remove();
+
+  // Define predefined romantic date ideas
+  const dateIdeas = [
+    "Long walk with your chit-chat",
+    "Binge night- TVD",
+    "Cooking together",
+    "Sit together in 335's Balcony",
+    "RailRoad jaie",
+    "Mast Alight Terrace par besiye",
+    "Decoring our Room for date itself",
+    "Go to bookstore - bookworm yuti",
+    "Hiking Oak mountain's trials",
+    "Candlelight Dinner, ekdam sexy",
+    "Candlelight Concert",
+    "Let's go to Arcade",
+    "Let's go to Top-Golf",
+    "Mast Olive Garden ma dinner Date",
+    "Bowling Alley ee javay ",
+    "Escape Room",
+    "Study Room ma besiye mast",
+    "Picnic",
+
+    // Add more date ideas as needed
+  ];
+
+  // Create and style a new button for Let's Go!
+  const letsGoBtn = document.createElement("button");
+  letsGoBtn.textContent = "Love You!";
+  letsGoBtn.classList.add("letsgo-btn"); // You can add a class for styling if needed
+  letsGoBtn.style.position = "absolute";
+
+  // Adjust the left position based on screen width
+  if (window.innerWidth <= 800) {
+    letsGoBtn.style.left = "95%";
+  } else {
+    letsGoBtn.style.left = "63%";
+  }
+
+  letsGoBtn.style.transform = "translate(-50%, -50%)";
+  letsGoBtn.style.width = "200px"; // Adjust the width as needed
+
+  // Add a click event listener to prompt the user with random romantic date ideas
+  letsGoBtn.addEventListener("click", () => {
+    const randomIndex = Math.floor(Math.random() * dateIdeas.length);
+    const selectedDateIdea = dateIdeas[randomIndex];
+
+    alert(`How about this romantic date idea: ${selectedDateIdea}`);
+  });
+
+  // Replace yesBtn with the new letsGoBtn
+  yesBtn.replaceWith(letsGoBtn);
+};
+
+const handleNoMouseOver = () => {
+  const { width, height } = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - width;
+  const maxY = window.innerHeight - height;
+
+  noBtn.style.left = `${Math.floor(Math.random() * maxX)}px`;
+  noBtn.style.top = `${Math.floor(Math.random() * maxY)}px`;
+};
+
+yesBtn.addEventListener("click", handleYesClick);
+noBtn.addEventListener("mouseover", handleNoMouseOver);
